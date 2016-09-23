03d2dfd219b839f06de8cf2b9164203b32375278
LuciusCao <lucius.cao@gmail.com>
2016-09-23 14:17:22 +0000

    added nietzsche.txt and modified path

--- text_gen.py
+++ text_gen.py
@@ -9,10 +9,7 @@ import numpy as np
 import random
 import sys
 
-try:
-    path = '~/.keras/datasets/nietzsche.txt'
-except FileNotFoundError:
-    path = get_file('nietzsche.txt', origin="https://s3.amazonaws.com/text-datasets/nietzsche.txt")
+path = './nietzsche.txt'
 text = open(path).read().lower()
 print('corpus length:', len(text))
 
